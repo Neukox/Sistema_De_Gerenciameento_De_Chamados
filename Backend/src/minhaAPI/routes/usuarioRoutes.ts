81a12a387c4e917fc834ea0e34ac06b52628c532
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-05-28 16:38:17 -0300

    Adiciona autenticação nas rotas de usuário

--- Backend/src/minhaAPI/routes/usuarioRoutes.ts
+++ Backend/src/minhaAPI/routes/usuarioRoutes.ts
@@ -1,19 +1,28 @@
 import express from "express";
 import userController from "../controllers/usuario.controllers";
 import { validateIdParam } from "../middlewares/validateParams";
+import authenticateToken from "../middlewares/autenticate";
 
 const router = express.Router();
 
 // Rota para atualizar informações do usuário
-router.get("/:id", validateIdParam, userController.getUserInfo);
+router.get(
+  "/:id",
+  [authenticateToken, validateIdParam],
+  userController.getUserInfo
+);
 
 // Rota para atualizar informações do usuário
-router.put("/:id", validateIdParam, userController.updateUserInfo);
+router.put(
+  "/:id",
+  [authenticateToken, validateIdParam],
+  userController.updateUserInfo
+);
 
 // Rota para alterar a senha do usuário
 router.patch(
   "/change-password/:id",
-  validateIdParam,
+  [authenticateToken, validateIdParam],
   userController.changePassword
 );
 
